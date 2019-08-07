4dcc4026d78a086ba481cebae54d55531288347e
Emilia <emiliacoleto@gmail.com>
2019-08-07 16:17:18 +0200

    pagina administrador

--- Etereo/gestionBD.php
+++ Etereo/gestionBD.php
@@ -3,8 +3,8 @@
 function crearConexionBD()
 {
 	$host="oci:dbname=localhost/XE;charset=UTF8";
-	$usuario="luismi";
-	$password="luismi";
+	$usuario="etereo";
+	$password="etereo";
 	
 	try{
 		/* Indicar que las sucesivas conexiones se puedan reutilizar */	
